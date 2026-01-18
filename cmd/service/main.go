fecb118d1491a7d40aabe4b092aa56109a64bc47
bondarenkozahar80-hub <bondarenkozahar80@gmail.com>
2026-01-18 14:38:13 +0300

    Add files via upload

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,1 +1,177 @@
+package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"Kafka-PostgreSQL-cache-test/config"
+	"Kafka-PostgreSQL-cache-test/internal/cache"
+	"Kafka-PostgreSQL-cache-test/internal/consumer"
+	"Kafka-PostgreSQL-cache-test/internal/repository"
+	"Kafka-PostgreSQL-cache-test/internal/server"
+	"Kafka-PostgreSQL-cache-test/migrations"
+	"go.uber.org/zap"
+)
+
+var (
+	cfgPath = "config/config.yaml"
+)
+
+func main() {
+	logger := initializeLogger()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	cfg := loadConfig(cfgPath, logger)
+
+	ordersRepo := initializeRepository(cfg, logger)
+	defer closeRepository(ordersRepo, logger)
+
+	//инициализация всех таблиц через одну схему
+	err := migrations.InitializeDatabaseSchema(ordersRepo.DB, logger)
+	if err != nil {
+		logger.Fatal("Failed to run migrations", zap.Error(err))
+	}
+
+	//инициализация таблиц бд через миграцию с версионированием
+	//migrationManager := migrations.NewMigrationManager(ordersRepo.DB, logger)
+	//if err := migrationManager.Up(); err != nil {
+	//	logger.Fatal("Failed to run migrations", zap.Error(err))
+	//}
+
+	appCache := initializeCache(cfg, ordersRepo, logger)
+	defer closeCache(appCache, logger)
+
+	httpServer := initializeController(cfg, appCache, logger)
+	startServer(httpServer, logger)
+
+	// Канал для системных сигналов
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		if err := consumer.Subscribe(
+			ctx,
+			appCache,
+			ordersRepo,
+			logger,
+			cfg.Kafka.Brokers,
+			cfg.Kafka.DlqTopic,
+		); err != nil {
+			logger.Error("Consumer error", zap.Error(err))
+		}
+	}()
+	logger.Info("Kafka consumer started")
+
+	<-sigchan
+	logger.Info("Received signal, shutting down...")
+
+	cancel()
+	_ = httpServer.Shutdown()
+
+	logger.Info("Application shut down gracefully")
+}
+
+func startServer(server *server.Server, logger *zap.Logger) {
+
+	go func() {
+		if err := server.Launch(); err != nil {
+			logger.Fatal("Server error", zap.Error(err))
+		}
+	}()
+
+	logger.Info("Server started successfully")
+}
+
+func initializeLogger() *zap.Logger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	return logger
+}
+
+func loadConfig(cfgPath string, logger *zap.Logger) *config.Config {
+	cfg, err := config.Load(cfgPath)
+	if err != nil {
+		logger.Fatal("Failed to load config", zap.Error(err))
+	}
+	logger.Info("Configuration loaded successfully")
+	return cfg
+}
+
+func initializeRepository(cfg *config.Config, logger *zap.Logger) *repository.OrdersRepo {
+	ordersRepo, err := repository.New(cfg)
+	if err != nil {
+		logger.Fatal("Failed to initialize repository", zap.Error(err))
+	}
+	logger.Info("Repository initialized successfully",
+		zap.String("host", cfg.DB.Host),
+		zap.String("port", cfg.DB.Port),
+		zap.String("db", cfg.DB.Name),
+		zap.String("user", cfg.DB.User),
+	)
+	return ordersRepo
+}
+
+func closeRepository(repo *repository.OrdersRepo, logger *zap.Logger) {
+	if err := repo.DB.Close(); err != nil {
+		logger.Error("Error closing repository", zap.Error(err))
+	} else {
+		logger.Info("Repository closed successfully")
+	}
+}
+
+func initializeCache(cfg *config.Config, ordersRepo *repository.OrdersRepo, logger *zap.Logger) cache.Cache {
+	// Создаем кэш на основе конфигурации
+	appCache, err := cache.New(cfg.Cache.ToCacheConfig())
+	if err != nil {
+		logger.Fatal("Failed to initialize cache", zap.Error(err))
+	}
+
+	// Загружаем заказы из БД в кэш
+	orders, err := ordersRepo.GetOrders()
+	if err != nil {
+		logger.Fatal("Orders Load error", zap.Error(err))
+	}
+
+	for _, order := range orders {
+		if err := appCache.SaveOrder(order); err != nil {
+			logger.Error("Failed to save order to cache",
+				zap.String("order_uid", order.OrderUID),
+				zap.Error(err))
+		} else {
+			logger.Info("Order cached successfully",
+				zap.String("order_uid", order.OrderUID))
+		}
+	}
+
+	logger.Info("Cache initialized successfully",
+		zap.Int("orders_loaded", len(orders)),
+		zap.String("cache_type", string(cfg.Cache.Type)))
+
+	return appCache
+}
+
+func closeCache(appCache cache.Cache, logger *zap.Logger) {
+	if err := appCache.Close(); err != nil {
+		logger.Error("Error closing cache", zap.Error(err))
+	} else {
+		logger.Info("Cache closed successfully")
+	}
+}
+
+func initializeController(cfg *config.Config, appCache cache.Cache, logger *zap.Logger) *server.Server {
+	httpServer, err := server.New(cfg, appCache, logger)
+	if err != nil {
+		logger.Fatal("Controller initialization error", zap.Error(err))
+	}
+	logger.Info("Controller initialized successfully")
+	return httpServer
+}
